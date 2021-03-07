ac27712feb230106b2ce62593fe37e982223505e
NelsonWang <wangnelson2@gmail.com>
2021-03-07 15:29:14 -0500

    Quiz | Passing Callbacks - Fixed

--- flutter_complete_guide/lib/answer.dart
+++ flutter_complete_guide/lib/answer.dart
@@ -3,14 +3,16 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
   final Function selectHandler;
   Answer(this.selectHandler);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: RaisedButton(
         color: Colors.blue,
+        textColor: Colors.white,
         child: Text("Answer 1"),
-        onPressed: this.selectHandler,
+        onPressed: () => selectHandler("Answer 1"),
       ),
     );
   }

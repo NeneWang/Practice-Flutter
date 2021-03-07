7ee03eb38c7cea071fe9f732f4886637420cbf02
NelsonWang <wangnelson2@gmail.com>
2021-03-06 20:49:27 -0500

    Quiz | Passing functions as references

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -7,12 +7,28 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion(answer) {
+    print("Answer chosen! $answer");
+  }
+
+  var questions = [
+    "What's your favorite color?",
+    "What's your favorite animal?"
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("My First App")),
-        body: Text("This is my Default Text"),
+        appBar: AppBar(title: Text("My First Quiz App")),
+        body: Column(
+          children: [
+            Text("The question!"),
+            RaisedButton(
+                child: Text("Answer 1"), onPressed: () => answerQuestion("Q1")),
+            RaisedButton(child: Text("Answer 2"), onPressed: null),
+            RaisedButton(child: Text("Answer 3"), onPressed: null),
+          ],
+        ),
       ),
     );
   }

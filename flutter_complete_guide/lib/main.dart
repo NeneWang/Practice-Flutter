1f44d9398ec28b9fbafba2d22df80f7899e0d0d8
NelsonWang <wangnelson2@gmail.com>
2021-03-07 01:13:45 -0500

    Quiz | StatefulWidget

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -6,15 +6,27 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   void answerQuestion(answer) {
     print("Answer chosen! $answer");
+    questionNumber++;
   }
 
   var questions = [
     "What's your favorite color?",
     "What's your favorite animal?"
   ];
+
+  var questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,7 +34,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: Text("My First Quiz App")),
         body: Column(
           children: [
-            Text("The question!"),
+            Text(questions.elementAt(questionNumber)),
             RaisedButton(
                 child: Text("Answer 1"), onPressed: () => answerQuestion("Q1")),
             RaisedButton(

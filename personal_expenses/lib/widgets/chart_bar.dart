b5d51d772178efea5e6c7b255ac0cbbd2efab0de
NelsonWang <wangnelson2@gmail.com>
2021-03-20 00:15:01 -0400

    Chart | Better fitted box + Reversed dates

--- personal_expenses/lib/widgets/chart_bar.dart
+++ personal_expenses/lib/widgets/chart_bar.dart
@@ -10,8 +10,11 @@ class ChartBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
-      FittedBox(
-        child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+      Container(
+        height: 20,
+        child: FittedBox(
+          child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        ),
       ),
       SizedBox(
         height: 4,

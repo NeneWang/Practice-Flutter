adcd8605fdccbcf4aaefcb752a63f0c542e23bef
NelsonWang <wangnelson2@gmail.com>
2021-03-21 17:30:47 -0400

    Delete | Button modification

--- personal_expenses/lib/widgets/new_transaction.dart
+++ personal_expenses/lib/widgets/new_transaction.dart
@@ -51,10 +51,15 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return SingleChildScrollView(
+        child: Card(
       elevation: 5,
       child: Container(
-        padding: EdgeInsets.all(10),
+        padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
@@ -99,6 +104,6 @@ class _NewTransactionState extends State<NewTransaction> {
           ],
         ),
       ),
-    );
+    ));
   }
 }

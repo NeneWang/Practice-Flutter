c365aeb81ef31495be73ee96d935b85a3f96c33e
NelsonWang <wangnelson2@gmail.com>
2021-03-27 12:40:54 -0400

    Meals App | Fonts

--- meals/lib/categories_screen.dart
+++ meals/lib/categories_screen.dart
@@ -8,6 +8,7 @@ class CategoriesScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: const Text("DeliMeal")),
         body: GridView(
+          padding: const EdgeInsets.all(25),
           children: DUMMY_CATEGORIES
               .map((catData) => CategoryItem(catData.title, catData.color))
               .toList(),

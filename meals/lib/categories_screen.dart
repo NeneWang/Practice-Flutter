faa70d58b7fe9b6439e6304f88285791ee9a1cb7
NelsonWang <wangnelson2@gmail.com>
2021-03-27 12:11:58 -0400

    Meals App | Dummy Data

--- meals/lib/categories_screen.dart
+++ meals/lib/categories_screen.dart
@@ -1,1 +1,12 @@
+import "package:flutter/material.dart";
 
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: <Widget>[],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200, childAspectRatio: 1.5),
+    );
+  }
+}

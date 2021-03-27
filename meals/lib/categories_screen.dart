132b0272f346e62f6bab530cd9d049aaf31647a4
NelsonWang <wangnelson2@gmail.com>
2021-03-27 12:37:41 -0400

    Meals App | Category Screen | Implementation

--- meals/lib/categories_screen.dart
+++ meals/lib/categories_screen.dart
@@ -5,12 +5,17 @@ import 'category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map((catData) => CategoryItem(catData.title, catData.color))
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200, childAspectRatio: 1.5, mainAxisSpacing: 20),
-    );
+    return Scaffold(
+        appBar: AppBar(title: Text("DeliMeal")),
+        body: GridView(
+          children: DUMMY_CATEGORIES
+              .map((catData) => CategoryItem(catData.title, catData.color))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 1.5,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+        ));
   }
 }

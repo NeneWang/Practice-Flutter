b3c471e2dd7a2b58943f05d00d4068d9adba29f0
NelsonWang <wangnelson2@gmail.com>
2021-03-27 12:25:18 -0400

    Meals App | Category Screen

--- meals/lib/categories_screen.dart
+++ meals/lib/categories_screen.dart
@@ -1,12 +1,16 @@
 import "package:flutter/material.dart";
+import 'dummy_data.dart';
+import 'category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: <Widget>[],
+      children: DUMMY_CATEGORIES
+          .map((catData) => CategoryItem(catData.title, catData.color))
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200, childAspectRatio: 1.5),
+          maxCrossAxisExtent: 200, childAspectRatio: 1.5, mainAxisSpacing: 20),
     );
   }
 }

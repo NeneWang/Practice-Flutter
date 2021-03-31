72e576e62388241975f50ab158ea55ae4163341d
NelsonWang <wangnelson2@gmail.com>
2021-03-31 19:32:47 -0400

    Routing | onGenerate, onUnknownRoute

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -36,6 +36,13 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
     );
   }
 }

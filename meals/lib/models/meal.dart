abe57ace08128cabf358d5dc5dcf7d6cf442fec7
NelsonWang <wangnelson2@gmail.com>
2021-03-31 13:08:22 -0400

    categories | Meal Model

--- meals/lib/models/meal.dart
+++ meals/lib/models/meal.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
+
 enum Complexity { Simple, Challanging, Hard }
+enum Affortability { Affordable, Pricey, Luxurious }
 
 class Meal {
   final String id;
@@ -9,4 +12,24 @@ class Meal {
   final List<String> steps;
   final int duration;
   final Complexity complexity;
+  final Affortability affortability;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
+
+  Meal(
+      {@required this.id,
+      @required this.categoryIds,
+      @required this.title,
+      @required this.imageUrl,
+      @required this.ingredients,
+      @required this.steps,
+      @required this.duration,
+      @required this.complexity,
+      @required this.affortability,
+      @required this.isGlutenFree,
+      @required this.isLactoseFree,
+      @required this.isVegan,
+      @required this.isVegetarian});
 }

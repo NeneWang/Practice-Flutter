123f7eb07fef1107837632c53f602c382fc631f9
NelsonWang <wangnelson2@gmail.com>
2021-03-31 13:10:40 -0400

    categories | MDummy Moddels

--- meals/lib/models/meal.dart
+++ meals/lib/models/meal.dart
@@ -1,33 +1,33 @@
 import 'package:flutter/foundation.dart';
 
 enum Complexity { Simple, Challanging, Hard }
-enum Affortability { Affordable, Pricey, Luxurious }
+enum Affordability { Affordable, Pricey, Luxurious }
 
 class Meal {
   final String id;
-  final List<String> categoryIds;
+  final List<String> categories;
   final String title;
   final String imageUrl;
   final List<String> ingredients;
   final List<String> steps;
   final int duration;
   final Complexity complexity;
-  final Affortability affortability;
+  final Affordability affordability;
   final bool isGlutenFree;
   final bool isLactoseFree;
   final bool isVegan;
   final bool isVegetarian;
 
-  Meal(
+  const Meal(
       {@required this.id,
-      @required this.categoryIds,
+      @required this.categories,
       @required this.title,
       @required this.imageUrl,
       @required this.ingredients,
       @required this.steps,
       @required this.duration,
       @required this.complexity,
-      @required this.affortability,
+      @required this.affordability,
       @required this.isGlutenFree,
       @required this.isLactoseFree,
       @required this.isVegan,

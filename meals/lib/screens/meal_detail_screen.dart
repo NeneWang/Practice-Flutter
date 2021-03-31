b9be1fbf097d9dd548c4061e41c7bb68c2c82ebf
NelsonWang <wangnelson2@gmail.com>
2021-03-31 19:11:40 -0400

    categories | Meal Details

---
+++ meals/lib/screens/meal_detail_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/meal-detail';
+
+  @override
+  Widget build(BuildContext context) {
+    final mealId = ModalRoute.of(context).settings.arguments as String;
+    return Center(child: Text("The meal! $mealId"));
+  }
+}

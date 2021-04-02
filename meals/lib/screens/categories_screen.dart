cf81c59d868e63f3b1b4fcc63bccf7c85d36f6d9
NelsonWang <wangnelson2@gmail.com>
2021-04-01 20:17:55 -0400

    Screens | TabScreen Redundancy

--- meals/lib/screens/categories_screen.dart
+++ meals/lib/screens/categories_screen.dart
@@ -7,9 +7,6 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeal'),
-      ),
       body: GridView(
         padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES

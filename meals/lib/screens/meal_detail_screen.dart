7209832416c4f638b974efcbbb9dde7ba66c02ee
NelsonWang <wangnelson2@gmail.com>
2021-04-02 13:39:30 -0400

    Screens | Detail | Delete pop

--- meals/lib/screens/meal_detail_screen.dart
+++ meals/lib/screens/meal_detail_screen.dart
@@ -86,6 +86,14 @@ class MealDetailScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.delete,
+        ),
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+      ),
     );
   }
 }

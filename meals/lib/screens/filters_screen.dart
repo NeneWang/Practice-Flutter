ba889c1673987e48af9c53918560da8f519faf36
NelsonWang <wangnelson2@gmail.com>
2021-04-02 17:23:13 -0400

    Screens | Filters | Filterscreen

--- meals/lib/screens/filters_screen.dart
+++ meals/lib/screens/filters_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FilterScreen extends StatefulWidget {
   static const routeName = '/filters';
+  @override
+  _FilterScreenState createState() => _FilterScreenState();
+}
 
+class _FilterScreenState extends State<FilterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(

d00a68bdcf408690ff326b61f101c541948318b5
NelsonWang <wangnelson2@gmail.com>
2021-04-02 19:05:50 -0400

    Setup | model product

---
+++ shop/lib/models/product.dart
@@ -0,0 +1,11 @@
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+
+  bool isFavorite;
+
+  Product(this.id, this.title, this.description, this.price, this.imageUrl);
+}

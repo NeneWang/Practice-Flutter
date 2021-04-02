b77f5fc93a236bd7933f85cb053942e23ce6de10
NelsonWang <wangnelson2@gmail.com>
2021-04-01 22:06:00 -0400

    Screens | Drawer | Column Main Drawer

--- meals/lib/widgets/main_drawer.dart
+++ meals/lib/widgets/main_drawer.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
+  Widget buildListTitle(String title, IconData icon) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 24,
+            fontWeight: FontWeight.bold),
+      ),
+      onTap: () {
+        // ..
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -11,11 +30,20 @@ class MainDrawer extends StatelessWidget {
           width: double.infinity,
           padding: EdgeInsets.all(20),
           alignment: Alignment.centerLeft,
+          color: Theme.of(context).accentColor,
           child: Text(
             'Cooking Up!',
-            style: TextStyle(fontWeight: FontWeight.w900),
+            style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).primaryColor),
           ),
-        )
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        buildListTitle("Meals", Icons.restaurant),
+        buildListTitle("Filters", Icons.settings),
       ],
     ));
   }

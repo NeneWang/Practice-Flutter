a60f162618f7d9386b5d72a398bc049cc12c0f32
NelsonWang <wangnelson2@gmail.com>
2021-04-01 21:58:00 -0400

    Screens | Drawer | Main Drawer

---
+++ meals/lib/widgets/main_drawer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: Column(
+      children: <Widget>[
+        Container(
+          height: 120,
+          width: double.infinity,
+          padding: EdgeInsets.all(20),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Cooking Up!',
+            style: TextStyle(fontWeight: FontWeight.w900),
+          ),
+        )
+      ],
+    ));
+  }
+}

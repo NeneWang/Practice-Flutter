f2bc13dca2e273bedb447e23294c9fb6235bdfad
NelsonWang <wangnelson2@gmail.com>
2021-04-01 20:33:51 -0400

    Screens | Bottom Bar

--- meals/lib/screens/tabs_screen.dart
+++ meals/lib/screens/tabs_screen.dart
@@ -8,6 +8,19 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Widget> _pages = [
+    CategoriesScreen(),
+    FavoriteScreen(),
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -15,21 +28,10 @@ class _TabsScreenState extends State<TabsScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: "Categories",
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: "Favorites",
-              ),
-            ],
-          ),
         ),
-        body: null,
+        body: _pages[_selectedPageIndex],
         bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
           backgroundColor: Theme.of(context).primaryColor,
           items: [
             BottomNavigationBarItem(

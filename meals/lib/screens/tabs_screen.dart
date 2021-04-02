c529653a1252baebf8dcd257385bc9bcdb613244
NelsonWang <wangnelson2@gmail.com>
2021-04-01 21:53:49 -0400

    Screens | Drawer | Basics

--- meals/lib/screens/tabs_screen.dart
+++ meals/lib/screens/tabs_screen.dart
@@ -32,6 +32,7 @@ class _TabsScreenState extends State<TabsScreen> {
         appBar: AppBar(
           title: Text(_pages[_selectedPageIndex]['title']),
         ),
+        drawer: Drawer(child: Text('The Drawer!')),
         body: _pages[_selectedPageIndex]['page'],
         bottomNavigationBar: BottomNavigationBar(
           onTap: _selectPage,

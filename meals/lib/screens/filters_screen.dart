ca1ae2060bd94b9edad8921d6be0cc646163f787
NelsonWang <wangnelson2@gmail.com>
2021-04-02 13:32:13 -0400

    Screens | Filters | Drawer

--- meals/lib/screens/filters_screen.dart
+++ meals/lib/screens/filters_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
@@ -9,6 +10,7 @@ class FiltersScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Your filters'),
       ),
+      drawer: MainDrawer(),
       body: Center(
         child: Text('Filters!'),
       ),

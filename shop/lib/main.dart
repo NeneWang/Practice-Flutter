90a38489e8d244951077210f25ab6b23c902c162
NelsonWang <wangnelson2@gmail.com>
2021-04-02 20:01:33 -0400

    Setup | Fonts | fonts

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
       title: 'MyShop',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        accentColor: Colors.deepOrange,
       ),
       home: ProductsOverviewScreen(),
     );

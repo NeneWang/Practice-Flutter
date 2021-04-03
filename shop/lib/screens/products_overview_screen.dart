0650f9af89ab9ca5a269482c03d531e144ccc478
NelsonWang <wangnelson2@gmail.com>
2021-04-02 21:14:48 -0400

    Setup | Grid Products as widget

--- shop/lib/screens/products_overview_screen.dart
+++ shop/lib/screens/products_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/product.dart';
 import '../widgets/product_item.dart';
+import '../widgets/product_grid.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   final List<Product> loadedProducts = [
@@ -40,22 +41,12 @@ class ProductsOverviewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    var scaffold = Scaffold(
       appBar: AppBar(
         title: Text("MyShop"),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10.0),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(loadedProducts[i].id,
-            loadedProducts[i].title, loadedProducts[i].imageUrl),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductsGrid(loadedProducts: loadedProducts),
     );
+    return scaffold;
   }
 }

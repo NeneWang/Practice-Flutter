3d4f95947260f490bd49b3f62cd0c3672460cadd
NelsonWang <wangnelson2@gmail.com>
2021-04-04 18:20:29 -0400

    Filters | Cart | Cart Screen

---
+++ shop/lib/screens/cart_screen.dart
@@ -0,0 +1,32 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Your Cart"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}

4848f48ce8de291811e877da7d16b57cec3d0fb3
NelsonWang <wangnelson2@gmail.com>
2021-04-04 17:35:54 -0400

    Filters | Cart | Models

---
+++ shop/lib/providers/cart.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem(
+      {@required this.id,
+      @required this.title,
+      @required this.quantity,
+      @required this.price});
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () =>
+            CartItem(id: DateTime.now().toString(), title: title, price: price),
+      );
+    }
+  }
+}

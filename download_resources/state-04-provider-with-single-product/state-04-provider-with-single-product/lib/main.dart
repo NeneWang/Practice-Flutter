d9c08c3ffec63b9de89419456d58d47abbca73b5
NelsonWang <wangnelson2@gmail.com>
2021-04-04 04:12:40 -0400

    Setup | Provider | likeable

--- download_resources/state-04-provider-with-single-product/state-04-provider-with-single-product/lib/main.dart
+++ download_resources/state-04-provider-with-single-product/state-04-provider-with-single-product/lib/main.dart
@@ -10,8 +10,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: Products(),
+    return ChangeNotifierProvider(
+      builder: (ctx) => Products(),
       child: MaterialApp(
           title: 'MyShop',
           theme: ThemeData(
--- download_resources/state-04-provider-with-single-product/state-04-provider-with-single-product/lib/widgets/products_grid.dart
+++ download_resources/state-04-provider-with-single-product/state-04-provider-with-single-product/lib/widgets/products_grid.dart
@@ -13,13 +13,13 @@ class ProductsGrid extends StatelessWidget {
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
       itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        create: (c) => products[i],
-        child: ProductItem(
-            // products[i].id,
-            // products[i].title,
-            // products[i].imageUrl,
-            ),
-      ),
+            builder: (c) => products[i],
+            child: ProductItem(
+                // products[i].id,
+                // products[i].title,
+                // products[i].imageUrl,
+                ),
+          ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 3 / 2,

1116c31a8766d6a7b724e686d472a751543db43d
NelsonWang <wangnelson2@gmail.com>
2021-04-06 22:21:17 +0200

    Orders | Importing and subtitle

--- shop/lib/widgets/order_item.dart
+++ shop/lib/widgets/order_item.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../providers/orders.dart' as ord;
+
 class OrderItem extends StatelessWidget {
+  final ord.OrderItem order;
+
+  OrderItem(this.order);
+
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(10),
       child: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          ListTile(
+            title: Text('\$ ${order.amount}'),
+            subtitle: Text(),
+          )
+        ],
       ),
     );
   }

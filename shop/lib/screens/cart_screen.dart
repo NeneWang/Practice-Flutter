3f21b7c6ef84c8c9c34d8c50471ff98b7339beea
NelsonWang <wangnelson2@gmail.com>
2021-04-05 21:17:50 -0400

    Filters | models | cart screen

--- shop/lib/screens/cart_screen.dart
+++ shop/lib/screens/cart_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../providers/cart.dart' show Cart;
 import '../widgets/cart_item.dart';
+import '../providers/orders.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -38,7 +39,11 @@ class CartScreen extends StatelessWidget {
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
                   FlatButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Provider.of<Orders>(context, listen: false).addOrder(
+                            cart.items.values.toList(), cart.totalAmount);
+                        cart.clear();
+                      },
                       child: Text('Order Now'),
                       textColor: Theme.of(context).primaryColor)
                 ],

6a47b3685991bd231e638a1ed0608c58272697b9
NelsonWang <wangnelson2@gmail.com>
2021-04-05 20:59:06 -0400

    Filters | Description

--- shop/lib/screens/product_detail_screen.dart
+++ shop/lib/screens/product_detail_screen.dart
@@ -36,6 +36,18 @@ class ProductDetailScreen extends StatelessWidget {
             Text(
               '\$${loadedProduct.price}',
               style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
             )
           ],
         ),

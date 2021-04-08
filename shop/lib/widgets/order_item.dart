f2ceff3ffdb211d7f2fe569dc8a012f9603a8144
NelsonWang <wangnelson2@gmail.com>
2021-04-08 02:12:10 -0400

    Orders | Order Screen | Better Time

--- shop/lib/widgets/order_item.dart
+++ shop/lib/widgets/order_item.dart
@@ -16,7 +16,8 @@ class OrderItem extends StatelessWidget {
         children: <Widget>[
           ListTile(
               title: Text('\$ ${order.amount}'),
-              subtitle: Text(DateFormat('dd MM yyyy').format(order.dateTime)),
+              subtitle:
+                  Text(DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime)),
               trailing:
                   IconButton(icon: Icon(Icons.expand_more), onPressed: () {})),
         ],

3adc6695f2736830b9ad4e94dfaff4bfba479753
NelsonWang <wangnelson2@gmail.com>
2021-04-08 23:26:33 -0400

    Orders | Order Screen | Sliding Delete confirmation

--- shop/lib/widgets/cart_item.dart
+++ shop/lib/widgets/cart_item.dart
@@ -37,6 +37,26 @@ class CartItem extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+              title: Text('Are you sure?'),
+              content: Text('Do you want to remove the item from the cart?'),
+              actions: <Widget>[
+                FlatButton(
+                    onPressed: () {
+                      Navigator.of(ctx).pop(false);
+                    },
+                    child: Text('No')),
+                FlatButton(
+                    onPressed: () {
+                      Navigator.of(ctx).pop(true);
+                    },
+                    child: Text('Yes')),
+              ]),
+        );
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },

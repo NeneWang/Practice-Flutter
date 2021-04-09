32a5c2945d4ed7e5819b2a5ac0721205db8ae64b
NelsonWang <wangnelson2@gmail.com>
2021-04-09 17:54:14 -0400

    Orders | Your products | Drawer and Styling

--- shop/lib/screens/user_products_screen.dart
+++ shop/lib/screens/user_products_screen.dart
@@ -4,6 +4,8 @@ import 'package:provider/provider.dart';
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
 
+import '../widgets/app_drawer.dart';
+
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
 
@@ -17,12 +19,18 @@ class UserProductsScreen extends StatelessWidget {
           IconButton(icon: const Icon(Icons.add), onPressed: () {})
         ],
       ),
+      drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
           itemCount: productsData.items.length,
-          itemBuilder: (_, i) => UserProductItem(
-              productsData.items[i].title, productsData.items[i].imageUrl),
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                  productsData.items[i].title, productsData.items[i].imageUrl),
+              Divider(),
+            ],
+          ),
         ),
       ),
     );

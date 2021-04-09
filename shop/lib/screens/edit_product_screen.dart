9491bef07641ae4a3e2c2db5cfc6ae1aaf81fe10
NelsonWang <wangnelson2@gmail.com>
2021-04-09 17:56:49 -0400

    Orders | Edit Product

---
+++ shop/lib/screens/edit_product_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  @override
+  _EditProductScreenState createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

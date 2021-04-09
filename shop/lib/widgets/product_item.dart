994c737286567f75bec2c9087b7897ef95d287f8
NelsonWang <wangnelson2@gmail.com>
2021-04-08 22:55:33 -0400

    Orders | Order Screen | Snack Feedback

--- shop/lib/widgets/product_item.dart
+++ shop/lib/widgets/product_item.dart
@@ -54,6 +54,13 @@ class ProductItem extends StatelessWidget {
             ),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text(
+                  'Added item to cart!',
+                  textAlign: TextAlign.center,
+                ),
+                duration: Duration(seconds: 2),
+              ));
             },
             color: Theme.of(context).accentColor,
           ),

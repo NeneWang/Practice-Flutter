1b2dffb227d773d7a733f3bce000beb0d0a78b6b
NelsonWang <wangnelson2@gmail.com>
2021-04-09 20:11:05 -0400

    Orders | Edit Product | Providers products implementation

--- shop/lib/providers/products.dart
+++ shop/lib/providers/products.dart
@@ -64,8 +64,16 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  void addProduct() {
+  void addProduct(Product product) {
     // _items.add(value);
+    final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        id: DateTime.now().toString());
+    _items.add(newProduct);
+    _items.insert(0, newProduct);
     notifyListeners();
   }
 }
--- shop/lib/screens/edit_product_screen.dart
+++ shop/lib/screens/edit_product_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import '../providers/product.dart';
+import '../providers/products.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
@@ -52,10 +55,11 @@ class _EditProductScreenState extends State<EditProductScreen> {
       return;
     }
     _form.currentState.save();
-    print(_editedProduct.title);
-    print(_editedProduct.description);
-    print(_editedProduct.price);
-    print(_editedProduct.imageUrl);
+    Provider.of<Products>(context, listen: false);
+    // print(_editedProduct.title);
+    // print(_editedProduct.description);
+    // print(_editedProduct.price);
+    // print(_editedProduct.imageUrl);
   }
 
   @override

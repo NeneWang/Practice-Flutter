9aaec518a6c79646deed747ee0e56c9e8071dd1b
NelsonWang <wangnelson2@gmail.com>
2021-04-10 13:01:39 -0400

    Orders | FIREBASE | Products provider | Fetching products in Init | Decode

--- shop/lib/providers/products.dart
+++ shop/lib/providers/products.dart
@@ -61,6 +61,7 @@ class Products with ChangeNotifier {
         'https://descartable-server-default-rtdb.firebaseio.com/products.json');
     try {
       final response = await http.get(url);
+      print(json.decode(response.body));
     } catch (error) {
       throw (error);
     }

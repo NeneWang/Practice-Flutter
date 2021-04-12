9496ab1b12be9e003be20d30df977f0e0a358587
NelsonWang <wangnelson2@gmail.com>
2021-04-12 00:23:22 -0400

    Orders | Refactoring | Order button

--- shop/lib/screens/cart_screen.dart
+++ shop/lib/screens/cart_screen.dart
@@ -38,20 +38,7 @@ class CartScreen extends StatelessWidget {
                     ),
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
-                  FlatButton(
-                    child: Text('ORDER NOW'),
-                    onPressed: cart.totalAmount <= 0
-                        ? null
-                        : () {
-                            Provider.of<Orders>(context, listen: false)
-                                .addOrder(
-                              cart.items.values.toList(),
-                              cart.totalAmount,
-                            );
-                            cart.clear();
-                          },
-                    textColor: Theme.of(context).primaryColor,
-                  )
+                  OrderButton(cart: cart)
                 ],
               ),
             ),
@@ -71,6 +58,32 @@ class CartScreen extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class OrderButton extends StatelessWidget {
+  const OrderButton({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Text('ORDER NOW'),
+      onPressed: cart.totalAmount <= 0
+          ? null
+          : () {
+              Provider.of<Orders>(context, listen: false).addOrder(
+                cart.items.values.toList(),
+                cart.totalAmount,
+              );
+              cart.clear();
+            },
+      textColor: Theme.of(context).primaryColor,
     );
   }
 }

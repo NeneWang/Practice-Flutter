202e55e322342759b00008e238f6da246ca7990c
NelsonWang <wangnelson2@gmail.com>
2021-04-12 14:23:55 -0400

    Auth | Auth Screen | Login Request

--- download_resources/Version Control/auth-02-signup/auth-02-signup/lib/main.dart
+++ download_resources/Version Control/auth-02-signup/auth-02-signup/lib/main.dart
@@ -7,13 +7,10 @@ import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
-
 import './providers/auth.dart';
 import './screens/orders_screen.dart';
-
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
-
 import './screens/auth_screen.dart';
 
 void main() => runApp(MyApp());

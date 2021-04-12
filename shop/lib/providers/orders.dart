6f2ad4ac21d53278d446b46b038b86c968b69540
NelsonWang <wangnelson2@gmail.com>
2021-04-11 23:44:54 -0400

    Orders | FIREBASE | Orders add Firebase basic

--- shop/lib/providers/orders.dart
+++ shop/lib/providers/orders.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/foundation.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/http_exception.dart';
 
 import './cart.dart';
 
@@ -23,16 +26,36 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(
-      0,
-      OrderItem(
-        id: DateTime.now().toString(),
-        amount: total,
-        dateTime: DateTime.now(),
-        products: cartProducts,
-      ),
-    );
-    notifyListeners();
+  void addOrder(List<CartItem> cartProducts, double total) async {
+    final url = Uri.parse(
+        'https://descartable-server-default-rtdb.firebaseio.com/orders.json');
+    final timestamp = DateTime.now();
+
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'amount': total,
+          'dateTime': timestamp.toIso8601String(),
+          'products': "something",
+        }),
+      );
+
+      final responseDecoded = json.decode(response.body);
+
+      _orders.insert(
+        0,
+        OrderItem(
+          id: DateTime.now().toString(),
+          amount: total,
+          dateTime: DateTime.now(),
+          products: cartProducts,
+        ),
+      );
+      notifyListeners();
+    } catch (error) {
+      print(error);
+      throw (error);
+    }
   }
 }

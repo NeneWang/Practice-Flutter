e72368af5ff2c37d80703ee0966c15ce0d7998b5
NelsonWang <wangnelson2@gmail.com>
2021-04-21 17:34:48 -0400

    Animations | Orders Page | Dropdown Animation

--- download_resources/Version Control/animations-07-finished/animations-07-finished/lib/widgets/order_item.dart
+++ download_resources/Version Control/animations-07-finished/animations-07-finished/lib/widgets/order_item.dart
@@ -44,31 +44,29 @@ class _OrderItemState extends State<OrderItem> {
             AnimatedContainer(
               duration: Duration(milliseconds: 300),
               padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: _expanded
-                  ? min(widget.order.products.length * 20.0 + 10, 100)
-                  : 0,
+              height: _expanded ? min(widget.order.products.length * 20.0 + 10, 100) : 0,
               child: ListView(
                 children: widget.order.products
                     .map(
                       (prod) => Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            prod.title,
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                prod.title,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '${prod.quantity}x \$${prod.price}',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.grey,
+                                ),
+                              )
+                            ],
                           ),
-                          Text(
-                            '${prod.quantity}x \$${prod.price}',
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey,
-                            ),
-                          )
-                        ],
-                      ),
                     )
                     .toList(),
               ),

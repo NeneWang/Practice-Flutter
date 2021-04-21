53c67bb9e8afbd79a7bd469f53d9dcc7b1322f8f
NelsonWang <wangnelson2@gmail.com>
2021-04-21 18:10:54 -0400

    Animations | Custom | Everything with Custom Transition

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -15,6 +15,7 @@ import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/splash_screen.dart';
 import './screens/auth_screen.dart';
+import './helpers/custom_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -47,10 +48,13 @@ class MyApp extends StatelessWidget {
           builder: (ctx, auth, _) => MaterialApp(
               title: 'MyShop',
               theme: ThemeData(
-                primarySwatch: Colors.purple,
-                accentColor: Colors.deepOrange,
-                fontFamily: 'Lato',
-              ),
+                  primarySwatch: Colors.purple,
+                  accentColor: Colors.deepOrange,
+                  fontFamily: 'Lato',
+                  pageTransitionsTheme: PageTransitionsTheme(builders: {
+                    TargetPlatform.android: CustomPageTransitionBuilder(),
+                    TargetPlatform.iOS: CustomPageTransitionBuilder(),
+                  })),
               home: auth.isAuth
                   ? ProductsOverviewScreen()
                   : FutureBuilder(

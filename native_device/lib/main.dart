41f78a8249958904e416769c38ebc89942d75b38
NelsonWang <wangnelson2@gmail.com>
2021-04-22 01:15:57 -0400

    native | Add Places Screen

--- native_device/lib/main.dart
+++ native_device/lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './providers/great_places.dart';
+import './screens/places_list_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-          child: MaterialApp(
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
         title: 'Great Places',
         theme: ThemeData(
           primarySwatch: Colors.indigo,
           accentColor: Colors.amber,
         ),
-        home: ...,
+        home: PlacesListScreen(),
       ),
     );
   }

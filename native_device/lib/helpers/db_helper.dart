a9baee0ab55ad31b36f3d34e059383f82adddba9
NelsonWang <wangnelson2@gmail.com>
2021-04-22 12:23:25 -0400

    native | sqflite | Create table places.db

--- native_device/lib/helpers/db_helper.dart
+++ native_device/lib/helpers/db_helper.dart
@@ -3,6 +3,10 @@ import 'package:path/path.dart' as path;
 
 class DBHelper {
   static Future<void> insert(String table, Map<String, Object> data) async {
-    sql.getDatabasesPath();
+    final dbpath = await sql.getDatabasesPath();
+    sql.openDatabase(path.join(dbpath, 'places.db'), onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)');
+    }, version: 1);
   }
 }

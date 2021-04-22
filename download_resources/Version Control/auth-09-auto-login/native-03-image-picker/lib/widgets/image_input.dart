f03b47da1fbfe2834ccba18993c8564aa61fecad
NelsonWang <wangnelson2@gmail.com>
2021-04-22 02:30:45 -0400

    native | Camera not working

--- download_resources/Version Control/auth-09-auto-login/native-03-image-picker/lib/widgets/image_input.dart
+++ download_resources/Version Control/auth-09-auto-login/native-03-image-picker/lib/widgets/image_input.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
   @override
@@ -9,19 +12,18 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File _storedImage;
-  // File _image;
-  final picker = ImagePicker();
+
   Future<void> _takePicture() async {
-    final imageFile = await picker.getImage(
+    final imageFile = await ImagePicker.pickImage(
       source: ImageSource.camera,
+      maxWidth: 600,
     );
     setState(() {
-      if (imageFile != null) {
-        _storedImage = File(imageFile.path);
-      } else {
-        print("No Image Selected");
-      }
+      _storedImage = imageFile;
     });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await imageFile.copy('${appDir.path}/$fileName');
   }
 
   @override
@@ -29,17 +31,21 @@ class _ImageInputState extends State<ImageInput> {
     return Row(
       children: <Widget>[
         Container(
-          width: 200,
+          width: 150,
           height: 100,
-          decoration:
-              BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
           child: _storedImage != null
               ? Image.file(
                   _storedImage,
                   fit: BoxFit.cover,
                   width: double.infinity,
                 )
-              : Text('No Image taken'),
+              : Text(
+                  'No Image Taken',
+                  textAlign: TextAlign.center,
+                ),
           alignment: Alignment.center,
         ),
         SizedBox(
@@ -52,7 +58,7 @@ class _ImageInputState extends State<ImageInput> {
             textColor: Theme.of(context).primaryColor,
             onPressed: _takePicture,
           ),
-        )
+        ),
       ],
     );
   }

16dea087e13bfb3756e4a1e49fd286f54b6813db
NelsonWang <wangnelson2@gmail.com>
2021-04-22 01:13:28 -0400

    native | basic screens setup

--- native_device/lib/main.dart
+++ native_device/lib/main.dart
@@ -5,13 +5,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber,
+    return ChangeNotifierProvider(
+          child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: ...,
       ),
-      home: ...,
     );
   }
 }

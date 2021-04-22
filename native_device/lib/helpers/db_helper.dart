56b4477d3cfbadb55983b7f8c6bddea891f33953
NelsonWang <wangnelson2@gmail.com>
2021-04-22 12:26:50 -0400

    native | sqflite | Inserting place | Helper

--- native_device/lib/helpers/db_helper.dart
+++ native_device/lib/helpers/db_helper.dart
@@ -4,9 +4,13 @@ import 'package:path/path.dart' as path;
 class DBHelper {
   static Future<void> insert(String table, Map<String, Object> data) async {
     final dbpath = await sql.getDatabasesPath();
-    sql.openDatabase(path.join(dbpath, 'places.db'), onCreate: (db, version) {
+    final sqlDb = await sql.openDatabase(path.join(dbpath, 'places.db'),
+        onCreate: (db, version) {
       return db.execute(
           'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)');
     }, version: 1);
+
+    await sqlDb.insert(table, data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
   }
 }

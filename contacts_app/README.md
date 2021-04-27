580dd53624c54d99086f896635e022c08ea8a4b7
NelsonWang <wangnelson2@gmail.com>
2021-04-27 03:47:19 -0400

    Yotuube Firebase tuto | Firestore Tuto!

---
+++ contacts_app/README.md
@@ -0,0 +1,3 @@
+# Firebase Firestore CRUD Part One
+
+### This is a simple project that demonstrates how to add and retrieve valur from Firebase Firestore.
---
+++ contacts_app/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:firebase_core/firebase_core.dart';
+import "package:flutter/material.dart";
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirebaseAuthDemo(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class FirebaseAuthDemo extends StatelessWidget {
+  final TextEditingController _textEditingController = TextEditingController();
+  final CollectionReference collectionReference = FirebaseFirestore.instance.collection('contacts');
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 50),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.7,
+                  child: TextFormField(
+                    controller: _textEditingController,
+                    style: TextStyle(fontSize: 22, color: Colors.black),
+                    decoration: InputDecoration(
+                      hintText: "Name",
+                      hintStyle: TextStyle(fontSize: 22, color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                SizedBox(
+                  child: ElevatedButton(
+                      onPressed: () async {
+                        await collectionReference.add({'name': _textEditingController.text}).then((value) => _textEditingController.clear());
+                      },
+                      child: Text(
+                        'Add Data',
+                        style: TextStyle(fontSize: 20),
+                      )),
+                ),
+              ],
+            ),
+            Expanded(
+                child: StreamBuilder(stream: collectionReference.snapshots(),
+                  builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if(snapshot.hasData){
+                    return ListView(
+                      children: snapshot.data.docs.map((e) => Column(
+                        children: [
+                          ListTile(title: Text(e['name']),),
+                          Divider(color: Colors.black.withOpacity(0.6), thickness: 2,)
+                        ],
+                      )).toList(),
+                    );
+                  }
+                  return Center(child: CircularProgressIndicator(),);
+                  },
+
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}

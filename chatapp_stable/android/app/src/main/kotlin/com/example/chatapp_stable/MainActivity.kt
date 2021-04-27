3a617074cad35ed0da8bb0234426a9ebde8ee042
NelsonWang <wangnelson2@gmail.com>
2021-04-27 01:20:05 -0400

    Creating stable

---
+++ chatapp_stable/android/app/src/main/kotlin/com/example/chatapp_stable/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatapp_stable
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ chatv1226att1/android/app/src/main/kotlin/com/example/chatv1226att1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatv1226att1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

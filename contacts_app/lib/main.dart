8d2c3934fbf166a742d72c30fb16164fb996ac85
NelsonWang <wangnelson2@gmail.com>
2021-04-29 20:00:00 -0600

    Date:      Thu Apr 29 20:00:00 2021 -0600
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   lib/main.dart

--- contacts_app/lib/main.dart
+++ contacts_app/lib/main.dart
@@ -6,7 +6,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
-  Text("Stuff");
+  Text("Placeholder");
 }
 
 class MyApp extends StatelessWidget {

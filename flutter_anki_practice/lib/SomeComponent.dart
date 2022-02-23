93bf1965289febbd90a13924de3bc2cba53da5ec
NelsonWang <wangnelson2@gmail.com>
2022-02-23 11:52:48 -0500

    stateful widget

---
+++ flutter_anki_practice/lib/SomeComponent.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SomeComponent extends StatelessWidget {
+  SomeComponent({
+    required this.foo,
+    this.bar = 'some string',
+  });
+
+  final String foo;
+  final String bar;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('$foo $bar'),
+    );
+  }
+}
---
+++ flutter_anki_practice/lib/WidgetWithState.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class WidgetWithState extends StatefulWidget {
+  @override
+  _WidgetWithStateState createState() => _WidgetWithStateState();
+}
+
+class _WidgetWithStateState extends State<WidgetWithState> {
+  int counter = 0;
+
+  increment() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  decrement() {
+    setState(() {
+      counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        FlatButton(onPressed: increment, child: Text('Increment')),
+        FlatButton(onPressed: decrement, child: Text('Decrement')),
+        Text(counter.toString()),
+      ],
+    );
+  }
+}
